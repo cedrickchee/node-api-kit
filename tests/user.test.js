d1c9bad74d2e82618b5a89e3de19cbee4eefdce7
Cedric Chee <cedric@invictusbyte.com>
2020-06-25 17:07:23 +0800

    Jest setup and teardown
    * Use Jest lifecycle methods to run some code before and after
    a test case.
    * Test login success and login failure.

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,5 +1,17 @@
 const request = require('supertest');
 const app = require('../src/app');
+const User = require('../src/models/user');
+
+const userOne = {
+  name: 'Mike',
+  email: 'mike@example.com',
+  password: '98!what!!!'
+};
+
+beforeEach(async () => {
+  await User.deleteMany();
+  await new User(userOne).save();
+});
 
 test('Should signup a new user', async () => {
   await request(app).post('/users').send({
@@ -8,3 +20,17 @@ test('Should signup a new user', async () => {
     password: 'hello!123',
   }).expect(201);
 });
+
+test('Should login existing user', async () => {
+  await request(app).post('/users/login').send({
+    email: userOne.email,
+    password: userOne.password,
+  }).expect(200);
+});
+
+test('Should not login nonexisting user', async () => {
+  await request(app).post('/users/login').send({
+    email: userOne.email,
+    password: 'this-is-not-my-password',
+  }).expect(400);
+})
