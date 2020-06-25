68bcc0d7128033f9d17c1b19d560c21170322471
Cedric Chee <cedric@invictusbyte.com>
2020-06-25 20:33:14 +0800

    Add task data & more user data

--- tests/fixtures/db.js
+++ tests/fixtures/db.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const User = require('../../src/models/user');
+const Task = require('../../src/models/task');
+
+// User data
 
 const userOneId = new mongoose.Types.ObjectId();
 const userOne = {
@@ -15,9 +18,50 @@ const userOne = {
   ],
 };
 
+const userTwoId = new mongoose.Types.ObjectId();
+const userTwo = {
+  _id: userTwoId,
+  name: 'Ming Yen',
+  email: 'mingyen@example.com',
+  password: '289!dev!!!',
+  tokens: [
+    {
+      token: jwt.sign({ _id: userTwoId }, process.env.JWT_SECRET),
+    },
+  ],
+};
+
+// Task data
+
+const taskOne = {
+  _id: new mongoose.Types.ObjectId(),
+  description: 'Test task 1',
+  completed: false,
+  owner: userOne._id,
+};
+
+const taskTwo = {
+  _id: new mongoose.Types.ObjectId(),
+  description: 'Test task 2',
+  completed: true,
+  owner: userOne._id,
+};
+
+const taskThree = {
+  _id: new mongoose.Types.ObjectId(),
+  description: 'Test task 3',
+  completed: true,
+  owner: userTwo._id,
+};
+
 const setupDatabase = async () => {
   await User.deleteMany();
+  await Task.deleteMany();
   await new User(userOne).save();
+  await new User(userTwo).save();
+  await new Task(taskOne).save();
+  await new Task(taskTwo).save();
+  await new Task(taskThree).save();
 };
 
 module.exports = {
