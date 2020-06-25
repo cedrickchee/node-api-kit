8a5a9b365e25c0c241d0aae71148001b530dae2a
Cedric Chee <cedric@invictusbyte.com>
2020-06-25 18:23:33 +0800

    Reformat code

--- tests/user.test.js
+++ tests/user.test.js
@@ -10,9 +10,11 @@ const userOne = {
   name: 'Mike',
   email: 'mike@example.com',
   password: '98!what!!!',
-  tokens: [{
-    token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET),
-  }],
+  tokens: [
+    {
+      token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET),
+    },
+  ],
 };
 
 beforeEach(async () => {
@@ -21,26 +23,35 @@ beforeEach(async () => {
 });
 
 test('Should signup a new user', async () => {
-  await request(app).post('/users').send({
-    name: 'Belinda Ling',
-    email: 'belinda@foo.bar',
-    password: 'hello!123',
-  }).expect(201);
+  await request(app)
+    .post('/users')
+    .send({
+      name: 'Belinda Ling',
+      email: 'belinda@foo.bar',
+      password: 'hello!123',
+    })
+    .expect(201);
 });
 
 test('Should login existing user', async () => {
-  await request(app).post('/users/login').send({
-    email: userOne.email,
-    password: userOne.password,
-  }).expect(200);
+  await request(app)
+    .post('/users/login')
+    .send({
+      email: userOne.email,
+      password: userOne.password,
+    })
+    .expect(200);
 });
 
 test('Should not login nonexisting user', async () => {
-  await request(app).post('/users/login').send({
-    email: userOne.email,
-    password: 'this-is-not-my-password',
-  }).expect(400);
-})
+  await request(app)
+    .post('/users/login')
+    .send({
+      email: userOne.email,
+      password: 'this-is-not-my-password',
+    })
+    .expect(400);
+});
 
 test('Should get profile for user', async () => {
   await request(app)
@@ -51,10 +62,7 @@ test('Should get profile for user', async () => {
 });
 
 test('Should not get profile for unauthenticated user', async () => {
-  await request(app)
-    .get('/users/me')
-    .send()
-    .expect(401);
+  await request(app).get('/users/me').send().expect(401);
 });
 
 test('Should delete account for user', async () => {
@@ -66,8 +74,5 @@ test('Should delete account for user', async () => {
 });
 
 test('Should not delete account for unauthenticated user', async () => {
-  await request(app)
-    .delete('/users/me')
-    .send()
-    .expect(401);
+  await request(app).delete('/users/me').send().expect(401);
 });
