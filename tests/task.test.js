d4358c32e02975b471dc6455f0dfb655b4865e8e
Cedric Chee <cedric@invictusbyte.com>
2020-06-26 09:08:46 +0800

    Clearer test case name & more assertions

--- tests/task.test.js
+++ tests/task.test.js
@@ -19,7 +19,7 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-test('Should create task for user', async () => {
+test('Should create task for authenticated user', async () => {
   const response = await request(app)
     .post('/tasks')
     .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
@@ -30,7 +30,8 @@ test('Should create task for user', async () => {
 
   const task = await Task.findById(response.body._id);
   expect(task).not.toBeNull();
-  expect(task.completed).toEqual(false);
+  expect(task.description).toBe('Task 1 from test');
+  expect(task.completed).toBe(false);
 });
 
 test('Should fetch user tasks', async () => {
@@ -40,10 +41,17 @@ test('Should fetch user tasks', async () => {
     .send()
     .expect(200);
 
-  expect(response.body.length).toEqual(2);
+  // Assertion to check the number of tasks for a user
+  expect(response.body.length).toBe(2); // can also use toEqual
+
+  // Assertion to check if the completed property is set to false by default
+  expect(response.body[0].completed).toBe(false);
+
+  // Assertion to check if the task is updated in the database
+  expect(response.body[0].description).toBe(taskOne.description);
 });
 
-test('Should not delete other users tasks', async () => {
+test('Should not allow user to delete other users task', async () => {
   const response = await request(app)
     .delete(`/tasks/${taskOne._id}`)
     .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
