94b67700164e8d61249d879eb47dcd4fff16196f
Cedric Chee <cedric@invictusbyte.com>
2020-06-26 10:18:37 +0800

    Fix bug & get failing test to pass

--- src/routers/task.js
+++ src/routers/task.js
@@ -85,7 +85,7 @@ router.patch('/tasks/:id', auth, async (req, res) => {
     });
 
     if (!task) {
-      res.status(404).send();
+      return res.status(404).send();
     }
 
     updates.forEach((update) => (task[update] = req.body[update]));
